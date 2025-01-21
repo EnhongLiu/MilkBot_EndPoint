fb0a9c633513fbc7a2c2afbdec17934ac2462fb7
EnhongLiu <el839@cornell.edu>
2025-01-21 15:21:51 -0700

    Create main_request.py

---
+++ main_request.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+# Endpoint URL and Token
+endpoint_url = "https://adb-65044996157806.6.azuredatabricks.net/serving-endpoints/milkbot_python/invocations"
+api_token = "dapi7390b3f7a8386b3831c19675b7503579-3"
+
+# Headers
+headers = {
+    "Authorization": f"Bearer {api_token}",
+    "Content-Type": "application/json"
+}
+
+parity1_para= requests.post(endpoint_url, headers=headers, data=json.dumps(parity1)).json()
+
+parity2_para= requests.post(endpoint_url, headers=headers, data=json.dumps(parity2)).json()
+
+parity3_para= requests.post(endpoint_url, headers=headers, data=json.dumps(parity3)).json()
